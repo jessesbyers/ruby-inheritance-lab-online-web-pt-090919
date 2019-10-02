bd0ba98044302242bf66751037e3152ad4cd1d35
Jesse Byers <jesse.s.byers@gmail.com>
2019-10-02 14:31:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,16 @@
 class Student < User
-  
+
+  def new
+    @knowledge_array = []
+  end
+
+  def learn(knowledge_string)
+    @knowledge_array << knowledge_string
+  end
+
+  def knowledge
+    @knowledge_array
+  end
+
 
 end
